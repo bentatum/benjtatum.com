82184833c2315aa2fd1f6c1c9f1e6ea57d9370f8
Ben Tatum <ben@clinc.com>
2021-08-12 11:08:18 -0400

    add link to yieldscience and adjust font sizes a bit

--- web/src/components/layout/Layout.svelte
+++ web/src/components/layout/Layout.svelte
@@ -5,8 +5,10 @@
 <div class="bg-yellow-500 text-black dark:bg-black dark:text-yellow-500 min-h-screen relative">
 	<div class="h-screen grid place-items-center z-10 relative">
 		<div class="text-center">
-			<h1 class="text-lg select-none font-black uppercase tracking-widest uppercase">"Website"</h1>
-			<div class="text-sm">
+			<h1 class="text-lg sm:text-2xl select-none font-black uppercase tracking-widest uppercase">
+				"Website"
+			</h1>
+			<div class="text-base sm:text-xl">
 				<MyLinksNav />
 			</div>
 		</div>
