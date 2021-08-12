d75170eb566031798ed712975961bfddaf6ad969
Ben Tatum <ben@clinc.com>
2021-08-12 11:16:07 -0400

    brighten up the yellow

--- web/src/components/layout/Layout.svelte
+++ web/src/components/layout/Layout.svelte
@@ -2,7 +2,7 @@
 	import MyLinksNav from '../MyLinksNav.svelte';
 </script>
 
-<div class="bg-yellow-500 text-black dark:bg-black dark:text-yellow-500 min-h-screen relative">
+<div class="bg-yellow-300 text-black dark:bg-black dark:text-yellow-300 min-h-screen relative">
 	<div class="h-screen grid place-items-center z-10 relative">
 		<div class="text-center">
 			<h1 class="text-lg sm:text-2xl select-none font-black uppercase tracking-widest uppercase">
